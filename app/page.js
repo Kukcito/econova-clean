9082d325e5162b0d03f9ff6a178f0786949a0f68
Kukcito <omar.v.ku@gmail.com>
2025-09-02 14:23:14 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import CTA from './components/CTA'
 import FAQ from './components/FAQ'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import Image from 'next/image'
 
 export default function Page(){
   return (
@@ -16,6 +17,14 @@ export default function Page(){
       <OnView />
       <Navbar />
       <Hero />
+      <section className="section container" data-animate>
+        <div className="lead" style={{marginBottom:10,color:'#9fb3c8'}}>Confían en nosotros</div>
+        <div style={{display:'grid',gridTemplateColumns:'repeat(5,1fr)',gap:16,alignItems:'center'}}>
+          {['cliente1','cliente2','cliente3','aliado1','aliado2'].map((n,i)=>(
+            <Image key={i} src={`/logos/${n}.svg`} alt={`Logo ${n}`} width={140} height={34}/>
+          ))}
+        </div>
+      </section>
       <KPIs />
       <Services />
       <Methodology />
