04b12dabfc02ccefa451ab896ab82cee54fccfc6
Kukcito <omar.v.ku@gmail.com>
2025-09-02 11:52:22 -0600

    Update Clients.js

--- app/content/Clients.js
+++ app/content/Clients.js
@@ -1,16 +1,8 @@
-import { clients } from '../content/home.data';
-
 export default function Clients(){
   return (
     <section id="clientes" className="section container">
       <h2 className="h2">Clientes</h2>
-      <div className="logos">
-        {clients.map(c=>(
-          <div key={c.name} className="logo">
-            <img src={c.logo} alt={c.name} loading="lazy" />
-          </div>
-        ))}
-      </div>
+      <div className="card">Logos próximamente</div>
     </section>
   );
 }
