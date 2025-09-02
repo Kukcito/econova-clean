74f6b8ce6b1c7ac79ce2ebb4c3326a8387509b26
Kukcito <omar.v.ku@gmail.com>
2025-09-02 13:23:49 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import KPIs from './components/KPIs';
-import Services from './components/Services';
-import Methodology from './components/Methodology';
-import Radar4 from './components/Radar4';
-import CTA from './components/CTA';
-import FAQ from './components/FAQ';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar'
+import OnView from './components/OnView'
+import Hero from './components/Hero'
+import KPIs from './components/KPIs'
+import Services from './components/Services'
+import Methodology from './components/Methodology'
+import Radar4 from './components/Radar4'
+import CTA from './components/CTA'
+import FAQ from './components/FAQ'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
-export default function Page() {
+export default function Page(){
   return (
     <main>
+      <OnView />
       <Navbar />
       <Hero />
       <KPIs />
@@ -23,5 +25,5 @@ export default function Page() {
       <Contact />
       <Footer />
     </main>
-  );
+  )
 }
