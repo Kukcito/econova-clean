178aad06182c033f4b2ca6122351b022878e136b
Kukcito <omar.v.ku@gmail.com>
2025-09-02 23:13:54 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,8 +3,15 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import Methodology from './components/Methodology';
 import Radar4 from './components/Radar4';
+
+import About from './components/About';
+import Compliance from './components/Compliance';
+import Sectors from './components/Sectors';
+import Cases from './components/Cases';
+import Testimonials from './components/Testimonials';
 import FAQ from './components/FAQ';
 import Contact from './components/Contact';
+import Media from './components/Media'; // opcional
 
 export default function Page() {
   return (
@@ -13,6 +20,18 @@ export default function Page() {
       <Services />
       <Methodology />
       <Radar4 />
+
+      {/* Bloques extensos */}
+      <About />
+      <Compliance />
+      <Sectors />
+      <Cases />
+      <Testimonials />
+
+      {/* Galería opcional */}
+      <Media />
+
+      {/* Cierre */}
       <FAQ />
       <Contact />
     </>
